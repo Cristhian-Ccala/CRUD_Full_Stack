4fe36d59c388805dc8b4f52c2305b274caa827f3
cristhian <kcaladj123@gmail.com>
2024-09-16 21:10:56 -0500

    creacion proyecto

---
+++ src/test/java/com/fullstack/crud_angular/CrudFullstackAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.crud_angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullstackAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
